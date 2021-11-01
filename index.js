f064f1e2ceec3f6afccbecc511119350f3582d06
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-01 13:32:52 -0400

    commit tree 8369823208373dd59f94f440338ff97dd82c3e07

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(location){
+    return Math.abs(hq - location);
+}
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+function distanceTravelledInFeet(a, b){
+    return Math.abs((a - b) * 264);
+}
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    } else if (distanceTravelledInFeet(start, destination) > 2500){
+        return "cannot travel that far";
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25;
+    }
+}
